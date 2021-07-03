f68da7c2a2351d190265707d9c016f156a65c36a
Natalie S <66453648+nsxcode@users.noreply.github.com>
2021-07-03 13:38:16 -0400

    Deleted the default project code

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <Profile name="Raaid" age="22" description="Comp Sci Student"/>
         <Profile name="Natalie" age="20" description="Mermaid in a human body"/>
       </header>
