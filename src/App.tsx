b8c448d942ef0df8f3e32eab130e088e83f81de2
Raaid <raaidsm@gmail.com>
2021-07-03 15:32:54 -0400

    Removed unnecessary tags from App

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,15 @@ import './App.css';
 import Profile from "./components/Profile";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h3>The Profiles</h3>
-        <Profile name="Raaid" age={22} description="Comp Sci Student"/>
-        <Profile name="Natalie" age={20} description="Mermaid in a human body"/>
-      </header>
-    </div>
-  );
+    return (
+        <main className="App">
+            <h3>The Profiles</h3>
+            <div className="profile-container">
+                <Profile name="Raaid" age={22} description="Comp Sci Student"/>
+                <Profile name="Natalie" age={20} description="Mermaid in a human body"/>
+            </div>
+        </main>
+    );
 }
 
 export default App;
