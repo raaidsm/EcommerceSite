f9140a05ca8a300a94b8a6ebe1e06ec0d73f9db3
Raaid <raaidsm@gmail.com>
2021-07-10 14:40:16 -0400

    Fix some minor formatting issues

--- src/components/ProfileSection.tsx
+++ src/components/ProfileSection.tsx
@@ -7,31 +7,29 @@ const ProfileSection: FC = props => {
     const onDragOver: DragEventHandler<HTMLDivElement> = (event) => {
         event.preventDefault();
         const dragging = document.querySelector(".dragging");
-        const afterElement: Element = getDraggableAfterElement(profileSectionDiv.current, event.clientY);
+        const afterElement: Element | undefined = getDraggableAfterElement(profileSectionDiv.current, event.clientY);
         if (afterElement === undefined) profileSectionDiv.current.appendChild(dragging);
         else profileSectionDiv.current.insertBefore(dragging, afterElement);
     };
 
-    const getDraggableAfterElement = (container: HTMLDivElement, y: number) : Element => {
+    const getDraggableAfterElement = (container: HTMLDivElement, y: number) : Element | undefined => {
         const draggableElements: Element[] = [...container.querySelectorAll(".draggable:not(.dragging)")];
 
         interface AfterElementReducerResult {
             offset: number;
-            previousElement?: Element
+            previousElement?: Element;
         }
 
-        const afterElement: Element | undefined = draggableElements.reduce((previousResult: AfterElementReducerResult, currentElement: Element) => {
+        return draggableElements.reduce((previousResult: AfterElementReducerResult, currentElement: Element) => {
             const box: DOMRect = currentElement.getBoundingClientRect();
             const offset: number = y - box.top - box.height / 2;
             if (offset < 0 && offset > previousResult.offset) {
                 return { offset: offset, previousElement: currentElement };
             }
             else {
-                return previousResult
+                return previousResult;
             }
         }, { offset: Number.NEGATIVE_INFINITY }).previousElement;
-
-        return afterElement!;
     };
 
     return <div>
