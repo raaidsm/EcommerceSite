c8326d9567f8679cfc4a7bc3b919adb038a6361f
Natalie S <66453648+nsxcode@users.noreply.github.com>
2021-07-11 22:59:37 -0400

    Creating a responsive header in progress

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,21 +1,31 @@
 import {FC} from "react";
+import React, {useState} from 'react'
 import "./Header.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 interface HeaderProps {
 
 }
 
 const Header: FC<HeaderProps> = (props: any) => {
+    const [showLinks, setShowLinks] = useState(false);
     return <div className="header">
-        <div className="logo">
+        <div className="headerLeft">
             <a href="#" className="logo">The React App :)</a>
         </div>
-           <header>
-               <a className="active"  href="#">Shop</a>
-               <a className="active" href="#">Blog</a>
-               <a className="active" href="#">About Us</a>
-           </header>
+        <div className="headerRight">
+            <div className="links" id={showLinks ? "hidden" : ""}>
+                <a href="#">Home</a>
+                <a href="#">About Us</a>
+                <a href="#">Shop</a>
+            </div>
+            <button onClick={ () => setShowLinks(!showLinks)}>Open</button>
+        </div>
+
     </div>;
+
+
 }
 
 export default Header;
