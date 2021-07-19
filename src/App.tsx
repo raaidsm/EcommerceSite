764f1867c2340831a05870b4992e6e8f3793fa88
Raaid <raaidsm@gmail.com>
2021-07-18 22:47:31 -0400

    Set up useReducer to track and render list of users and add to that list

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react';
+import {Reducer, useEffect, useReducer, useState} from 'react';
 import './App.css';
 import ProfileSection from "./components/ProfileSection";
 import Profile from "./components/Profile";
@@ -7,18 +7,41 @@ import NewProfile from "./components/new_profile/NewProfile";
 import UserInput from './components/new_profile/UserInput';
 
 function App() {
-    const [users, setUsers] = useState([
+    type State = {
+        users: Array<UserInput>;
+    }
+    enum ActionType {
+        ADD = "ADD",
+        REMOVE = "REMOVE"
+    }
+    type Action = {
+        type: ActionType;
+        payload: UserInput | number;
+    }
+    const reducer = (state: State, action: Action): State => {
+        let newState: State;
+        switch (action.type) {
+            case ActionType.ADD:
+                console.log("Adding profile: " + action.payload);
+                newState = { users: [...state.users, action.payload as UserInput] };
+                return newState;
+            case ActionType.REMOVE:
+                console.log("Remove profile at index " + action.payload);
+                newState = { users: state.users.filter((user, index) => index !== action.payload as number) };
+                return newState;
+        }
+    };
+    const [state, dispatch] = useReducer<Reducer<State, Action>>(reducer, { users: [
         { name: "Raaid", age: 22, description: "Comp Sci Student"},
         { name: "Natalie", age: 20, description: "Mermaid in a human body"},
         { name: "Chris", age: 26, description: "Cat Man >:)"},
         { name: "Brandon", age: 20, description: "don't talk to me"}
-    ]);
+    ] });
 
     const addProfileHandler = (profile: UserInput) => {
+        //DEBUGGING
         console.log("New Profile = " + profile);
-        let tempUsers = users;
-        tempUsers.push(profile);
-        setUsers(tempUsers);
+        dispatch({ type: ActionType.ADD, payload: profile });
     };
 
     return (
@@ -26,7 +49,7 @@ function App() {
             <Header></Header>
             <ProfileSection>
                 {
-                    users.map((user, index) => {
+                    state.users.map((user: UserInput, index: number) => {
                         return <Profile key={index} name={user.name} age={user.age} description={user.description} />
                     })
                 }
--- src/components/new_profile/ProfileData.tsx
+++
@@ -1,8 +0,0 @@
-interface ProfileData {
-    id: number;
-    name: string;
-    age: number;
-    desc: string;
-}
-
-export default ProfileData;
